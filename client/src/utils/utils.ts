4c092cf8e73fbf033f7f3d8745dd5d8fbe5ea796
jiaezzang <visveryver2@gmail.com>
2024-03-22 10:49:35 +0900

    feat: sprite animation을 실행하는 메서드 생성

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -56,3 +56,81 @@ export const calculateTriangleCenter = (
 
     return { x: centerX, y: centerY };
 };
+
+/** 스프라이트 애니메이션을 실행한다.
+ * @param param0 애니메이션 실행을 위해 필요한 객체
+ * @param param0.canvas 애니메이션을 그릴 canvas
+ * @param param0.imgSrc sprite 이미지의 경로
+ * @param param0.row sprite 이미지의 행
+ * @param param0.col sprite 이미지의 열
+ * @param param0.repeatCount sprite animation을 반복 실행할 횟수
+ */
+export const spriteAnimation = ({
+    canvas,
+    imgSrc,
+    row,
+    col,
+    repeatCount,
+}: {
+    canvas: HTMLCanvasElement;
+    imgSrc: string;
+    row: number;
+    col: number;
+    repeatCount: number;
+}) => {
+    const sprite = new Image();
+    sprite.src = imgSrc;
+    const ctx = canvas.getContext('2d');
+    if (!ctx) return;
+    sprite.onload = () => {
+        let animationId: number;
+        const frameWidth = sprite.width / col;
+        const frameHeight = sprite.height / row;
+        const ratio = frameHeight / frameWidth;
+        const frameCount = row * col;
+        canvas.width = canvas.offsetWidth;
+        canvas.height = canvas.offsetHeight;
+        let frame = 0;
+        let repeatCounter = 0; // 반복 횟수를 추적하기 위한 변수 추가
+        const animate = () => {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+            const _row = Math.floor(frame / col);
+            const _col = frame % col;
+            const sx = _col * frameWidth;
+            const sy = _row * frameHeight;
+
+            ctx.drawImage(
+                sprite,
+                sx,
+                sy,
+                frameWidth,
+                frameHeight,
+                0,
+                0,
+                canvas.width,
+                canvas.width * ratio
+            );
+            if (frame === frameCount - 1) {
+                if (repeatCounter < repeatCount - 1) {
+                    frame = 0;
+                    repeatCounter++;
+                } else {
+                    ctx.clearRect(
+                        0,
+                        0,
+                        canvas.offsetWidth,
+                        canvas.offsetHeight
+                    );
+                    cancelAnimationFrame(animationId);
+                    return;
+                }
+            } else {
+                frame = (frame + 1) % frameCount;
+            }
+            animationId = requestAnimationFrame(animate);
+        };
+
+        animate();
+    };
+};
